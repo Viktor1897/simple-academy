f56fb6b9ae7ad07897c3256ffc01e1c9f09d046a
Viktar Kolasau <vitykolosov@gmail.com>
2023-03-14 21:37:36 +0100

    update LoginForm structure

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,9 +1,9 @@
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { Button, Grid, IconButton, InputAdornment, TextField } from "@mui/material";
-import { lightBlue } from "@mui/material/colors";
+import { Button, IconButton, InputAdornment, Paper, TextField } from "@mui/material";
+import { Stack } from "@mui/system";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { auth } from "../../firebase/firebase";
@@ -19,7 +19,8 @@ const LoginForm = () => {
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-    const login = async () => {
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
         await signInWithEmailAndPassword(auth, email, password)
             .then(() => {
                 navigate(ADMINISTRATION_ROUTE);
@@ -31,55 +32,53 @@ const LoginForm = () => {
     };
 
     return (
-        <Grid
-            container
-            width="400px"
-            gap={2}
-            padding={2}
-            direction="column"
+        <Paper
+            elevation={4}
             sx={{
-                backgroundColor: lightBlue[50],
-                borderRadius: 2,
-                margin: "0 auto",
+                width: "400px",
+                padding: 2,
                 position: "absolute",
                 top: "50%",
                 left: "50%",
                 transform: "translate(-50%, -50%)",
             }}>
-            <TextField
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                label="Email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-                label="Password"
-                required
-                type={showPassword ? "text" : "password"}
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                InputProps={{
-                    endAdornment:
-            <InputAdornment position="end">
-                <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    edge="end"
-                >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-            </InputAdornment>
-
-                }}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button variant='outlined' onClick={login}>Login</Button>
-        </Grid>
+            <form onSubmit={handleSubmit}>
+                <Stack gap={3}>
+                    <TextField
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        label="Email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        label="Password"
+                        required
+                        type={showPassword ? "text" : "password"}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        InputProps={{
+                            endAdornment:
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button type="submit" variant='outlined'>Login</Button>
+                </Stack>
+            </form>
+        </Paper>
     );
 };
 
