cae243e14bcab93d4d696de89121c41172071cb9
viktar kolasau <vitykolosov@gmail.com>
2023-04-24 18:43:38 +0200

    configure responsiveness for InfoIconsSection

--- src/components/InfoIconsSection/InfoIconsSection.tsx
+++ src/components/InfoIconsSection/InfoIconsSection.tsx
@@ -1,4 +1,5 @@
 //TODO сделать нормальные svg или взять картинки нормального качества 
+import styled from "@emotion/styled";
 import barber from "assets/barber.png";
 import handshake from "assets/handshake.png";
 import razor from "assets/razor.png";
@@ -12,19 +13,19 @@ const InfoIconsSection = () => {
         <Section backgroundColor={COLORS.bgGray} padding="6.5rem 0">
             <ContentWrapper flexDirection="row" alignItems="start">
                 <VerticalFlexContainer>
-                    <img src={barber} alt="barber"/>
+                    <InfoIcon src={barber} alt="barber"/>
                     <Text>Szkoleniowcy - barberzy z 6+ letnim doświadczeniem</Text>
                 </VerticalFlexContainer>
                 <VerticalFlexContainer>
-                    <img src={razor} alt="razor"/>
+                    <InfoIcon src={razor} alt="razor"/>
                     <Text>Narzędzie, modele, kosmetyka - wszystko jest wliczone w koszt szkolenia</Text>
                 </VerticalFlexContainer>
                 <VerticalFlexContainer>
-                    <img src={handshake} alt="handshake"/>
+                    <InfoIcon src={handshake} alt="handshake"/>
                     <Text>Elastyczne warunki płatności. Możliwość rozłożenia ceny szkolenia na raty</Text>
                 </VerticalFlexContainer>
                 <VerticalFlexContainer>
-                    <img src={recruitment} alt="recruitment"/>
+                    <InfoIcon src={recruitment} alt="recruitment"/>
                     <Text>Wszysci studenci mają szansę dolączyć się do ekipy SIMPLE Barbershop</Text>
                 </VerticalFlexContainer>
             </ContentWrapper>
@@ -33,3 +34,8 @@ const InfoIconsSection = () => {
 };
 
 export default InfoIconsSection;
+
+const InfoIcon = styled.img`
+    width: 8rem;
+    heigh: 8rem;
+`;
